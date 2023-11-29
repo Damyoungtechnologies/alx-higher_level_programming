db5424b762de946dc354f2dceea65ff15189cf6d
damyoungtechnologies <damyounggraphics@gmail.com>
2023-11-29 02:18:13 -0800

    another C programming again?

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,32 +1,69 @@
-#include <stdio.h>
-
+#include "lists.h"
 #include <stdlib.h>
 
-/* Definition for singly-linked list */
-typedef struct listint {
-	int data;
-	struct listint* next;
-} listint_t;
+/**
+ * _realloc - Reallocates a memory block
+ * @ptr: The pointer to the previous memory block
+ * @old_size: The size of the old memory block
+ * @new_size: The size of the new memory block
+ *
+ * Return: The pointer to the new memory block otherwise NULL
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+	unsigned int min_size = old_size < new_size ? old_size : new_size;
+	unsigned int i;
 
-/* Function to check if there is a cycle in the linked list */
-int check_cycle(listint_t *list) {
-	listint_t *slow = list, *fast = list;
-	
-	while (slow != NULL && fast != NULL && fast->next != NULL) {
-		slow = slow->next;
-		fast = fast->next->next;
-
-        /* If there is a cycle, the fast pointer will eventually meet the slow pointer */
-        if (slow == fast) {
-            return 1;  /* Cycle detected */
-        }
-    }
-
-    return 0;  
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr != NULL)
+	{
+		if (new_size == 0)
+		{
+			free(ptr);
+			return (NULL);
+		}
+		new_ptr = malloc(new_size);
+		if (new_ptr != NULL)
+		{
+			for (i = 0; i < min_size; i++)
+				*((char *)new_ptr + i) = *((char *)ptr + i);
+			free(ptr);
+			return (new_ptr);
+		}
+		free(ptr);
+		return (NULL);
+	}
+	else
+	{
+		new_ptr = malloc(new_size);
+		return (new_ptr);
+	}
 }
 
-/* Example usage */
-int main() {
 
-    return 0;
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: list head
+ * Return: 0 if no, 1 if yes
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast, *slow = list;
+
+	if (list == NULL)
+		return (0);
+
+	fast = list->next;
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		if (slow == fast)
+			return (1);
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+	return (0);
 }
