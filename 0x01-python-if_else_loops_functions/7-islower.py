289e2e856ad2ca2f3406e372f7360b606a38d718
damyoungtechnologies <damyounggraphics@gmail.com>
2023-11-29 12:38:54 -0800

    jaus

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    return ord('a') <= ord(c) <= ord('z')
-
-result1 = islower('a')
-result2 = islower('Z')
-
-print(result1)  # Should print True
-print(result2)  # Should print False
-
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+             return False
