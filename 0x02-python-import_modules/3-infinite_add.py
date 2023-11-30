ebcf909b5620d9d94245f2b46637b2233e27824a
damyoungtechnologies <damyounggraphics@gmail.com>
2023-11-30 10:18:53 -0800

    hup

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
+
 def add_arg(argv):
 
     n = len(argv) - 1
 
     if n == 0:
-
         print("{:d}".format(n))
 
         return
-
     else:
-
         i = 1
 
         add = 0
-
         while i <= n:
 
             add += int(argv[i])
@@ -22,8 +19,6 @@ def add_arg(argv):
             i += 1
 
         print("{:d}".format(add))
-
-
 
 if __name__ == "__main__":
 
