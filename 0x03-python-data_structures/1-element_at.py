6726680d92d678c7d70f4151d4d613eda8aa14c0
damyoungtechnologies <damyounggraphics@gmail.com>
2023-12-02 13:27:42 -0800

    task 1 again

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return None
-    rain = len(my_list)
-    elif idx > rain:
-        return None
-
+    return my_list[idx]
