4b68464cadcd0c21c9dbfd2bcc0c9f5bfc5227e6
damyoungtechnologies <damyounggraphics@gmail.com>
2023-12-19 14:12:03 -0800

    just in case

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
 import sys
+
 
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except (ValueError, TypeError) as err:
-        print("Exception: {}".format(err), file=sys.stderr)
-        return False
-    else:
         return True
+    except(TypeError, ValueError) as mesg:
+        print("Exception: {}".format(mesg), file=sys.stderr)
+        return False
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
-
 import sys
 
+
 def safe_function(fct, *args):
+    result = 0
     try:
         result = fct(*args)
         return result
-    except Exception as err:
-        print("Exception: {}".format(err), file=sys.stderr)
+    except Exception as msg:
+        print("Exception: {}".format(msg), file=sys.stderr)
         return None
-
-# Example usage:
-# Define a function to test
-# def example_function(x, y):
-#     return x + y
-
-# Call the safe_function with the defined function and arguments
-# result = safe_function(example_function, 3, 4)
-# if result is not None:
-#     print("Result:", result)
-# else:
-#     print("Function execution failed")
